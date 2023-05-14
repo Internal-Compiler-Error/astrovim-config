7b432170fd3d2248c396a422594f445303a40fd7
Liang Wang <liang.wang.ice@gmail.com>
2023-05-14 17:55:00 -0400

    rice my shit even more

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,22 +4,37 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.rust" },
+
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.nightfox" },
+  { import = "astrocommunity.colorscheme.kanagawa" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.completion.copilot-lua" },
   { -- further customize the options set by the community
     "copilot.lua",
     opts = {
       suggestion = {
         keymap = {
-          accept = "<C-l>",
+          accept = "<S-l>",
           accept_word = false,
           accept_line = false,
-          next = "<C-.>",
-          prev = "<C-,>",
-          dismiss = "<C/>",
+          next = "<S-.>",
+          prev = "<S-,>",
+          dismiss = "<S/>",
         },
       },
+    },
+  },
+
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 120,
+      disabled_filetypes = { "help" },
     },
   },
 }
