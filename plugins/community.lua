8f2c82e626301c5beafb786a21cddc70abb21dac
Liang Wang <liang.wang.ice@gmail.com>
2023-09-13 20:49:40 -0400

    add ts support

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,7 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript" },
 
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.colorscheme.rose-pine" },
